2a74172c8fecdcd10bfb69386258d9068a78b87f
Raquel Muriega <muriega.raquel@gmail.com>
2021-06-09 19:55:44 -0300

    subiendo repo

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import Frase from './components/Frase';
+
+function App() {
+
+  useEffect(() =>{
+    consultarApi();
+  },[])
+  // ESTO DE ARRIBA SUPLE LA SOLUCION DE ABAJO
+  // const [frase, setFrase] = useState(consultarApi)
+
+  const [frase, setFrase] = useState({})
+
+  const consultarApi = async () => {
+    const url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+    const api = await fetch(url)
+    const frase = await api.json()
+    setFrase(frase[0]);
+  }
+
+
+  return (
+    <div>
+      <Contenedor>
+        <Frase frase={frase}/>
+        <Boton onClick={consultarApi}>
+          Obtener Frase
+          </Boton>
+      </Contenedor> 
+    </div>
+  );
+}
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;  
+`
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif; 
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 1px solid black;  
+  transition: background-size .8s ease;
+  :hover{
+    cursor: pointer;
+    background-size: 400px; 
+  }
+`;
+
+
+export default App;
